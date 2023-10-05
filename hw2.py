2c01adcf99ab4045bd98fe3d94638cd66d477407
Kimberly Dieu <kimberlytdieu@gmail.com>
2023-10-05 18:25:25 -0500

    GET request

--- hw2.py
+++ hw2.py
@@ -2,41 +2,71 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 import mysql.connector
-
-
+from mysql.connector import Error
 
 #database connection
-config = {
-    'user': 'admin',
-    'password': '12345678',
-    'host': 'cis3368fall.cplpsxg0kzbu.us-east-1.rds.amazonaws.com',  
-    'database': 'cis3368',
-    'raise_on_warnings': True
-}
+import mysql.connector
+from mysql.connector import Error
 
-try:
-    connection = mysql.connector.connect(**config)
+def create_con(hostname, username, pwd, dbname):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=hostname,
+            user=username,
+            password=pwd,
+            database=dbname
+        )
+        print("Success: Connected to the database")
+    except Error as e:
+        print("Error: The error occurred at:", e)
+    return connection
 
-    if connection.is_connected():
-        print("Connected to MySQL database")
-        
-        
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
-finally:
+conn = create_con('cis3368fall.cplpsxg0kzbu.us-east-1.rds.amazonaws.com', 'admin', '12345678', 'cis3368')
+cursor = conn.cursor(dictionary=True)
+sql = 'SELECT * FROM zoo'
+cursor.execute(sql)
+rows = cursor.fetchall()
 
-    if 'connection' in locals() and connection.is_connected():
-        connection.close()
-        print("Connection closed")
+# test printing data from table
+# for row in rows:
+#     print(row)
 
-
-
-
-# app= Flask(__name__)
-# @app.route('/')
-# def home():
-#     return "hey"
-
+cursor.close()
+conn.close()
 
 
-# app.run(port=7780)
+# GET REQUEST | return all animals from the zoo | running on http://127.0.0.1:5000/animals
+
+app = Flask(__name__)
+@app.route('/animals', methods=['GET'])
+def get_animals():
+    try:
+        connection = mysql.connector.connect(
+            host='cis3368fall.cplpsxg0kzbu.us-east-1.rds.amazonaws.com',
+            user='admin',
+            password='12345678',
+            database='cis3368'
+        )
+        cursor = connection.cursor(dictionary=True)
+
+        cursor.execute('SELECT * FROM zoo')
+        animals = cursor.fetchall()
+
+        return jsonify(animals)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+            
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# POST
+
+
